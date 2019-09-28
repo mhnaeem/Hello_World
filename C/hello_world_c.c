d44b461106d360e3136bb05a87e52212a403af4d
Muhammad Hammad <mhnaeem@mun.ca>
2019-09-28 15:44:12 -0230

    hello world in C added

---
+++ C/hello_world_c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("\nHi, I am Muhammad Hammad.\n");
+    printf("You can find more information about me at -> https://www.cs.mun.ca/~mhnaeem\n\n\n");
+    
+    printf("Hello World Project:\n");
+    printf("This is just a fun project, I am trying to add a hello world program in all the programming languages and frameworks I come in contact with.");
+   
+    return 0;
+}
