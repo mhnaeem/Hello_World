1d1e41382cbb417b0c0283af6e65407e80cb71aa
Muhammad Hammad <mhnaeem@mun.ca>
2019-09-28 15:32:38 -0230

    hello world in python added

---
+++ Python/hello_world_python.py
@@ -0,0 +1,7 @@
+if __name__== "__main__" :
+    print("\nHi, I am Muhammad Hammad.\n")
+    print("You can find more information about me at -> https://www.cs.mun.ca/~mhnaeem\n\n\n")
+    
+    print("Hello World Project:\n")
+    print("This is just a fun project, I am trying to add a hello world program in all the programming languages and frameworks I come in contact with.")
+    
