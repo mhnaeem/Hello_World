3a2de4761b71b4b1723d8a19273c263cb4a1190d
Muhammad Hammad <mhnaeem@mun.ca>
2019-09-28 15:39:43 -0230

    hello world in c++ added

---
+++ C++/hello_world_c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nHi, I am Muhammad Hammad.\n";
+    cout << "You can find more information about me at -> https://www.cs.mun.ca/~mhnaeem\n\n\n";
+    
+    cout << "Hello World Project:\n";
+    cout << "This is just a fun project, I am trying to add a hello world program in all the programming languages and frameworks I come in contact with.";
+   
+
+    return 0;
+}
