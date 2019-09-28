7fcfcaa6cd951c2387372cd14b8cd87dbf372b04
Muhammad Hammad <mhnaeem@mun.ca>
2019-09-28 15:57:23 -0230

    hello world added in JavaScript

---
+++ JavaScript/hello_world_javascript.js
@@ -0,0 +1,9 @@
+function helloWorld() {
+    console.log("\nHi, I am Muhammad Hammad.\n");
+    console.log("You can find more information about me at -> https://www.cs.mun.ca/~mhnaeem\n\n\n");
+
+    console.log("Hello World Project:\n");
+    console.log("This is just a fun project, I am trying to add a hello world program in all the programming languages and frameworks I come in contact with.");
+}
+
+helloWorld();
